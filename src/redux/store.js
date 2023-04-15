300c05e945219f861c7b504fdcf2b74854dd7643
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-15 15:58:35 +0300

    created store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { contactsReducer } from "redux/contacts/contactsSlice";
+import { filterReducer} from "redux/filter/filterSlice";
 
-
-
-// {
-//   contacts: [],
-//   filter: ""
-// }
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
