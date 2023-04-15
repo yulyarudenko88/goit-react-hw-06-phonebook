5eae623514a569627e64a07a4d70c39df2ea2a42
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-15 18:33:06 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
 import { Section } from 'components/Section/Section';
 import { AddContactsForm } from 'components/AddContactsForm/AddContactsForm';
 import { ContactsList } from 'components/ContactsList/ContactsList';
 import { Filter } from 'components/Filter/Filter';
 import { Wrapper } from './App.styled';
-
-// import { getContacts } from 'redux/contacts/selectors';
-import { getFilter } from 'redux/filter/selectors';
-// import { deleteContact } from 'redux/contacts/contactsSlice';
-import { setFilter } from 'redux/filter/filterSlice';
 
 // const parseContacts =
 //   JSON.parse(localStorage.getItem('userContacts')) === null
@@ -23,30 +17,19 @@ import { setFilter } from 'redux/filter/filterSlice';
 //     : JSON.parse(localStorage.getItem('userContacts'));
 
 export const App = () => {
-  const dispatch = useDispatch();
-  // const contacts = useSelector(getContacts);
-  const {filter} = useSelector(getFilter);
   
 
   // useEffect(() => {
   //   localStorage.setItem('userContacts', JSON.stringify(contacts));
   // }, [contacts]);
-
-  // const handleDeleteContact = contactId => dispatch(deleteContact(contactId));
-
-  const changeFilter = e => dispatch(setFilter(e.target.value));
-
-  // const visibleContacts = contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes(filter.toLowerCase())
-  // );
-
+  
   return (
     <Wrapper>
       <Section title="Phonebook">
         <AddContactsForm />
       </Section>
       <Section title="Contacts">
-        <Filter value={filter} onChange={changeFilter} />
+        <Filter />
         <ContactsList
         />
       </Section>
